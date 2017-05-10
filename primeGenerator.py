4c27b8163c532dc3796cf13bdbbf986047bd03c7
mameli <mameli93@gmail.com>
2017-05-10 09:30:51 +0200

    prime fix rsa

--- primeGenerator.py
+++ primeGenerator.py
@@ -1,16 +1,28 @@
 import random
 from rabin import rabinTest
+import timeit
 
 
-def isPrime(r):
-    baton = random.randint(1, r)
-    return not rabinTest(baton, r)
+def isPrime(r, k):
+    for _ in range(k):
+        baton = random.randint(1, r)
+        if rabinTest(baton, r) is True:
+            return False
+    return True
 
 
-def generate(bottom, top):
+def generate(bottom, top, k):
     r = random.randint(bottom, top)
-    while r % 2 == 0 or not isPrime(r):
+    n = 0
+    while r % 2 == 0 or not isPrime(r, k):
         r = random.randint(bottom, top)
     return r
 
-print generate(1000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000, 10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000)
+
+def wrapper():
+    generate(pow(10, 99), pow(10, 100), 16)
+
+# prime = generate(pow(10, 99), pow(10, 100), 32)
+# print prime
+# print 'cifre', len(str(prime))
+print (timeit.timeit(wrapper, number=1))
--- rabin.py
+++ rabin.py
@@ -2,19 +2,17 @@ from esponenziazioneVeloce import expVeloce
 
 
 def rabinTest(x, n):
-    if n % 2 == 0:
-        return True if n != 2 else False
     m = n - 1
     r = 0
     while m % 2 == 0:
         m //= 2
         r += 1
-    xr = pow(x, m, n)
+    xr = expVeloce(x, m, n)
     if xr == 1:
         return False
     # print 'x0 ', xr
     for i in range(r):
-        xr = pow(xr, 2, n)
+        xr = expVeloce(xr, 2, n)
         # print 'x' + str(i + 1), '', xr
         if xr == n - 1:
             return False
